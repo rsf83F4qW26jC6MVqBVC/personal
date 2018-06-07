98cfdb14d4db98f3daea8d9b18448fedf4488fa2
Neil Hodge <neil.hodge@gmail.com>
2018-06-06 17:40:53 -0700

    * Storage term completed * Diffusion term underway

--- driver.py
+++ driver.py
@@ -158,12 +158,76 @@ class mesh:
         ax.set_aspect('equal')
 
 
+    def UpdateKinematics(self,f):
+    
+        NumElems=self.elems.shape[1]
+
+        for iElem in range(NumElems):
+            ElemNodes=m.elems[:,iElem]
+            # NodeCoords=
+            # NodeField=
+# dNdx: elements,IntPoints,N,coords
+# dTdx: dNdx, T
+            for iip in range(NumIntPoints):        
+                dNdxi=ps.dNdxi[:,iip]
+                dNdeta=ps.dNdeta[:,iip]
+
+                dx_dxi=
+                dy_dxi=
+                dx_deta=
+                dy_deta=
+
+                for iNode in range(NumNodes):
+                    dN_dxi[0]=dNdxi[iNode]
+                    dN_dxi[1]=dNdeta[iNode]
+            #     J=[dx_dxi   dy_dxi]
+            #        dx_deta  dy_deta]
+            #     dNdx=np.linslg.solve(J,dNdxi)
+
+
+        self.dNdx=dNdx
+        self.dfdx=dfdx
+
+
+#==========================================================================
+class material:
+
+    def __init__(self,rho,cp):
+        self.rho=rho
+        self.cp=cp
+
+    # def InitMesh(self,NumEdgeElems):
+
+    #     coords=np.zeros(shape=((NumEdgeElems+1)*(NumEdgeElems+1),2))
+    #     elems=np.zeros(shape=(4,NumEdgeElems*NumEdgeElems),dtype=np.int64)
+
+    #     h=1.0/NumEdgeElems
+    #     iNode=-1
+    #     iElem=-1
+    #     for j in range(NumEdgeElems+1):
+    #         for i in range(NumEdgeElems+1):
+    #             # print("i={}".format(i))
+    #             iNode=iNode+1
+    #             coords[iNode,0]=i*h
+    #             coords[iNode,1]=j*h
+    #             if (i>0 and j>0):
+    #                 iElem=iElem+1
+    #                 elems[0,iElem]=iNode-(NumEdgeElems+1)-1
+    #                 elems[1,iElem]=iNode-(NumEdgeElems+1)
+    #                 elems[2,iElem]=iNode
+    #                 elems[3,iElem]=iNode-1
+    #             # if (16*6<iElem and iElem<16*7):
+    #                 # print("iElem={}, ElemNodes={}".format(iElem,elems[:,iElem]))
+
+    #     return coords,elems
+
+
 #==========================================================================
 class fields:
 
     def __init__(self,InitField,BCField,m,PlotInit=False):
         print("initializing field . . . ",end="")
-        self.DOFNums,self.NumActiveDOFs,self.fNP1,self.fdotNP1=self.InitField(InitField,BCField,m.coords)
+        self.InitField(InitField,BCField,m.coords)
         if (PlotInit):
             # pause("beginning of PlotInit")
             fig=plt.figure("field initialization",figsize=(9,9))
@@ -179,22 +243,35 @@ class fields:
         NumNodes=coords.shape[0]
         DOFNums=np.empty(shape=(NumNodes),dtype=np.int32)
         fNP1=np.empty(shape=(NumNodes),dtype=np.float64)
+        fN=np.empty(shape=(NumNodes),dtype=np.float64)
         fdotNP1=np.empty(shape=(NumNodes),dtype=np.float64)
+        fdotN=np.empty(shape=(NumNodes),dtype=np.float64)
         NumDOFs=0
         NumActiveDOFs=0
+        NumDirichletDOFs=0
         for iNode in range(NumNodes):
             NumDOFs=NumDOFs+1
             if (coords[iNode,1]<1.0e-05):
-                DOFNums[iNode]=-NumDOFs
+                NumDirichletDOFs=NumDirichletDOFs+1
+                DOFNums[iNode]=-NumDirichletDOFs
                 fNP1[iNode]=BCField
+                fN[iNode]=BCField
                 fdotNP1[iNode]=0.0
+                fdotN[iNode]=0.0
             else:
                 NumActiveDOFs=NumActiveDOFs+1
-                DOFNums[iNode]=NumDOFs
+                DOFNums[iNode]=NumActiveDOFs
                 fNP1[iNode]=InitField
+                fN[iNode]=InitField
                 fdotNP1[iNode]=0.0
+                fdotN[iNode]=0.0
 
-        return DOFNums,NumActiveDOFs,fNP1,fdotNP1
+        self.DOFNums=DOFNums
+        self.NumActiveDOFs=NumActiveDOFs
+        self.fNP1=fNP1
+        self.fN=fN
+        self.fdotNP1=fdotNP1
+        self.fdotN=fdotN
 
     def plot(self,m):
 
@@ -205,8 +282,8 @@ class fields:
         yplot=xplot
         xplot1,yplot1=np.meshgrid(xplot,yplot)
         zplot=self.fNP1.reshape(NumEdgeNodes,NumEdgeNodes)
-        print("after call to reshape:")
-        print("fNP1: {}".format(self.fNP1))
+        # print("after call to reshape:")
+        # print("fNP1: {}".format(self.fNP1))
         # print("fdotNP1: {}".format(self.fdotNP1))
         # print("zplot: {}".format(zplot))
 
@@ -216,6 +293,41 @@ class fields:
 
         ax=plt.gca()
         ax.set_aspect('equal')
+
+    def update(self,df,m,ti,dt):
+        NumNodes=m.coords.shape[0]
+        for iNode in range(NumNodes):
+            DOFNum=self.DOFNums[iNode]
+            if (DOFNum>0):
+                self.fNP1[iNode]=self.fNP1[iNode]+df[DOFNum-1]
+        self.fdotNP1=ti.update(dt,self)
+
+
+#==========================================================================
+class TimeInt:
+
+    def __init__(self,TimeIntType,alpha=0.0):
+        self.type=TimeIntType
+        self.InitTimeInt(TimeIntType,alpha)
+
+    def InitTimeInt(self,TimeIntType,alpha):
+        if (TimeIntType==1):
+            # generalized trapezoidat
+            self.alpha=alpha
+
+    def dTdot_dT(self,dt):
+        if (self.type==1):
+            alpha=self.alpha
+            dTdot_dT=1.0/(alpha*dt)
+
+        return dTdot_dT
+
+    def update(self,dt,f):
+        if (self.type==1):
+            alpha=self.alpha
+            fdotNP1=(f.fNP1-f.fN-dt*(1.0-alpha)*f.fdotN)/(alpha*dt)
+
+        return fdotNP1
 
 
 #==========================================================================
@@ -231,6 +343,10 @@ class LinearSystem:
     def zero(self):
         self.RHS.fill(0.0)
         self.LHS.fill(0.0)
+
+    def solve(self):
+        df=np.linalg.solve(self.LHS,self.RHS)
+        return df
 
 
 #==========================================================================
@@ -274,13 +390,126 @@ class LinearSystem:
 
 
 #==========================================================================
-def UpdateStorageTerm(m,f,ls):
+def UpdateStorageTerm(ps,m,mat,f,ls):
 
+    NumElemNodes=m.elems.shape[0]
     NumElems=m.elems.shape[1]
-    # for iElem in range(NumElems):
-        # print("updating storage term for iElem={} now . . .".format(iElem))
-        # for iip in range(NumIntPoints):
-        
+
+    ElemNodes=np.empty(shape=(NumElemNodes),dtype=np.int32)
+    ElemRHS=np.empty(shape=(NumElemNodes),dtype=np.float64)
+    ElemLHS=np.empty(shape=(NumElemNodes,NumElemNodes),dtype=np.float64)
+    # print("DOFNums={}".format(f.DOFNums))
+
+    for iElem in range(NumElems):
+        print("updating storage term for iElem={} now . . .".format(iElem))
+        PrintFlag=False
+        if (iElem%7==0):
+            PrintFlag=True
+        ElemNodes=m.elems[:,iElem]
+        NodeCoords=m.coords[ElemNodes,:]
+        NodeTemps=f.fNP1[ElemNodes]
+        NodeRates=f.fdotNP1[ElemNodes]
+        if (PrintFlag):
+            print("ElemNodes(iElem={})={}".format(iElem,ElemNodes))
+        ElemRHS.fill(0.0)
+        ElemLHS.fill(0.0)
+        for iip in range(NumIntPoints):
+            # int point field and rate
+            # IntPointTemp=
+            IntPointRate=np.dot(ps.N[:,iip],NodeRates)
+            # print("IntPointRate={}".format(IntPointRate))
+            # print("dt={}".format(dt))
+            dTdot_dT=ti.dTdot_dT(dt)
+            print("dTdot_dT={}".format(dTdot_dT))
+            Nlocal=ps.N[:,iip]
+
+            # RHS
+            ElemRHS=ElemRHS+Nlocal*mat.rho*mat.cp*IntPointRate
+            # LHS
+            ElemLHS=ElemLHS+np.outer(Nlocal,Nlocal)*mat.rho*mat.cp*dTdot_dT
+
+        if (PrintFlag):
+            print("ElemRHS(iElem={})={}".format(iElem,ElemRHS))
+            print("ElemLHS(iElem={})={}".format(iElem,ElemLHS))
+        ElemDOFs=f.DOFNums[ElemNodes]
+        if (PrintFlag):
+            print("ElemDOFs(iElem={})={}".format(iElem,ElemDOFs))
+        for iDOF in range(NumElemNodes):
+            iDOFNum=ElemDOFs[iDOF]
+            ls.RHS[iDOFNum-1]=ls.RHS[iDOFNum-1]+ElemRHS[iDOF]
+            for jDOF in range(NumElemNodes):
+                jDOFNum=ElemDOFs[jDOF]
+                ls.LHS[iDOFNum-1,jDOFNum-1]=ls.LHS[iDOFNum-1,jDOFNum-1]+ElemLHS[iDOF,jDOF]
+        if (PrintFlag):
+            print("")
+        # if (iElem==33):
+        #     sys.exit()
+        print("finished updating storage term for iElem={}".format(iElem))
+        print("")
+
+
+#==========================================================================
+def UpdateDiffusionTerm(ps,m,mat,f,ls):
+
+    NumElemNodes=m.elems.shape[0]
+    NumElems=m.elems.shape[1]
+
+    ElemNodes=np.empty(shape=(NumElemNodes),dtype=np.int32)
+    ElemRHS=np.empty(shape=(NumElemNodes),dtype=np.float64)
+    ElemLHS=np.empty(shape=(NumElemNodes,NumElemNodes),dtype=np.float64)
+    # print("DOFNums={}".format(f.DOFNums))
+
+    for iElem in range(NumElems):
+        PrintFlag=False
+        if (iElem%7==0):
+            PrintFlag=True
+        if (PrintFlag):
+            print("updating diffusion term for iElem={} now . . .".format(iElem))
+        ElemNodes=m.elems[:,iElem]
+        NodeCoords=m.coords[ElemNodes,:]
+        NodeTemps=f.fNP1[ElemNodes]
+        NodeRates=f.fdotNP1[ElemNodes]
+        if (PrintFlag):
+            print("ElemNodes(iElem={})={}".format(iElem,ElemNodes))
+        ElemRHS.fill(0.0)
+        ElemLHS.fill(0.0)
+        for iip in range(NumIntPoints):
+            # int point field and rate
+            # IntPointTemp=
+            IntPointRate=np.dot(ps.N[:,iip],NodeRates)
+            # print("IntPointRate={}".format(IntPointRate))
+            # print("dt={}".format(dt))
+            dTdot_dT=ti.dTdot_dT(dt)
+            print("dTdot_dT={}".format(dTdot_dT))
+            Nlocal=ps.N[:,iip]
+
+            m.UpdateKinematics(f)
+
+            # RHS
+            ElemRHS=ElemRHS+dNlocal_dx*mat.k*dTdx
+            # LHS
+            ElemLHS=ElemLHS+np.outer(dNlocal_dx,dNlocal_dx)*mat.k
+
+        if (PrintFlag):
+            print("ElemRHS(iElem={})={}".format(iElem,ElemRHS))
+            print("ElemLHS(iElem={})={}".format(iElem,ElemLHS))
+        ElemDOFs=f.DOFNums[ElemNodes]
+        if (PrintFlag):
+            print("ElemDOFs(iElem={})={}".format(iElem,ElemDOFs))
+        for iDOF in range(NumElemNodes):
+            iDOFNum=ElemDOFs[iDOF]
+            ls.RHS[iDOFNum-1]=ls.RHS[iDOFNum-1]+ElemRHS[iDOF]
+            for jDOF in range(NumElemNodes):
+                jDOFNum=ElemDOFs[jDOF]
+                ls.LHS[iDOFNum-1,jDOFNum-1]=ls.LHS[iDOFNum-1,jDOFNum-1]+ElemLHS[iDOF,jDOF]
+        if (PrintFlag):
+            print("")
+        # if (iElem==33):
+        #     sys.exit()
+        if (PrintFlag):
+            print("finished updating diffusion term for iElem={}".format(iElem))
+            print("")
+
 
 
 #==========================================================================
@@ -296,19 +525,25 @@ def plotter(m,f,time,PlotMesh,PlotField):
 #==========================================================================
 # user inputs
 #==========================================================================
-NumEdgeElems=13
+NumEdgeElems=3
+NumIntPoints=4
+
+# 316L
+rho=8.0e-06 # kg/mm^3
+cp=500.0 # J/kg-K
 
 InitialTemp=303.0
 BCTemp=505.0
 
 MaxNumSteps=10^10
+MaxNumSteps=2
 EndTime=1.0
 dt=0.1
-dt=0.5000000000000001
+TimeIntType=1 # generalized trapezoidal
+alpha=1.0
 
-MaxNonlinIters=20
+MaxNonlinIters=2
 
-NumIntPoints=4
 
 
 #==========================================================================
@@ -316,7 +551,9 @@ NumIntPoints=4
 #==========================================================================
 ps=ParametricSpace(NumIntPoints)
 m=mesh(NumEdgeElems,PlotInit=False)
+mat=material(rho,cp)
 f=fields(InitialTemp,BCTemp,m,PlotInit=False)
+ti=TimeInt(TimeIntType,alpha)
 ls=LinearSystem(f)
 
 fig=plt.figure("primary figure window",figsize=(11,9))
@@ -341,11 +578,12 @@ while True:
         iNonlin=iNonlin+1
 
         ls.zero()
-        UpdateStorageTerm(m,f,ls)
-        # UpdateDiffusionTerm
+        UpdateStorageTerm(ps,m,mat,f,ls)
+        UpdateDiffusionTerm(ps,m,mat,f,ls)
         # UpdateSourceTerm
-        # SolveLinearSystem
-        # UpdateFields
+        dT=ls.solve()
+        print("solution increment={}".format(dT))
+        f.update(dT,m,ti,dt)
         # CheckConvergnce
     
         if (iNonlin==MaxNonlinIters):
